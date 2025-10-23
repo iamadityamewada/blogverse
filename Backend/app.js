62a6dc35a1733da2774cfb465302d8a02ff6b27a
Aditya Mewada <eng.adityamewada@gmail.com>
2025-10-23 11:56:30 +0530

    app.js update

--- Backend/app.js
+++ Backend/app.js
@@ -4,13 +4,28 @@ const app = express();
 const { dbConnect } = require("./src/config/db");
 const { authRoute } = require("./src/router/auth");
 const { blogRouter } = require("./src/router/blog");
+
+// It's better practice to connect to the DB *before* starting to listen or exporting
+dbConnect();
+
 app.use(cors());
 app.use(express.json());
+
+// Define a simple root route for testing
+app.get('/', (req, res) => {
+  res.send('Backend API is running!');
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/blog", blogRouter);
 
+// Remove or comment out app.listen for Vercel
+/*
 app.listen(4000, () => {
   console.log("Server is Running at: http://localhost:4000");
-  dbConnect(); 
+  // dbConnect(); // Connect earlier
 });
+*/
+
+// Export the app for Vercel
+module.exports = app; // <= Add this line
